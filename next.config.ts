8e121adc383eb88d1e4a7d0988050a2070184cc4
tsakamaki4-alt <tsakamaki4@gmail.com>
2026-01-27 06:07:09 +0800

    Update v4.9

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig = {
   },
   // This matches your repository name exactly
  // basePath: '/lukz_erp_frontend',
-  //assetPrefix: '/lukz_erp_frontend',
 
   //trailingSlash: true,
 };
